f7c57f15e9ab743434830aca4b357727c4d06754
Marlon Marcello <marlon.marcello@gmail.com>
2023-11-07 13:27:46 -0800

    fix(docs): small tweak to tag;

--- apps/docs/stories/Tag/Tag.stories.tsx
+++ apps/docs/stories/Tag/Tag.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { Tag } from "@wethegit/components";
+import { Row, Tag } from "@wethegit/components";
 
 const meta: Meta<typeof Tag> = {
   component: Tag,
@@ -16,14 +16,9 @@ type Story = StoryObj<typeof Tag>;
 
 export const Default: Story = {
   render: (args) => (
-    <Tag {...args}>
-      Tempor anim duis velit ut ea occaecat ullamco tempor elit elit ullamco id
-      pariatur quis. Laboris incididunt aute ipsum pariatur. Et sit non et irure
-      laboris reprehenderit cupidatat aliqua. Ipsum ex aliqua magna commodo sint
-      sunt. Velit ad officia cupidatat ipsum. Ex nulla exercitation aliqua irure
-      sint ex sunt. Elit est sint ut tempor id sit dolore amet elit. Deserunt
-      esse sint elit nulla tempor. Laboris non laboris magna nulla incididunt id
-      est esse.
+    <Tag as={Row} {...args}>
+      This <code>&lt;Tag /&gt;</code> component is rendering a{" "}
+      <code>&lt;Row /&gt;</code> component.
     </Tag>
   ),
   name: "Tag",
