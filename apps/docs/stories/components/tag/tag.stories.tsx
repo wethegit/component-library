642b4bd2ce9a08601b2ff2c0621b8350683f82de
Marlon Marcello <marlon.marcello@gmail.com>
2023-11-14 14:26:42 -0800

    docs: fix paths, adds utilities docs

--- apps/docs/stories/components/tag/tag.stories.tsx
+++ apps/docs/stories/components/tag/tag.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react"
-import { Row, Tag } from "@wethegit/components"
+
+import { Tag } from "@wethegit/components"
 
 const meta: Meta<typeof Tag> = {
   title: "components/tag",
@@ -17,10 +18,10 @@ type Story = StoryObj<typeof Tag>
 
 export const Default: Story = {
   render: (args) => (
-    <Tag as={Row} {...args}>
+    <Tag as="p" {...args}>
       <span>
-        This <code>&lt;Tag /&gt;</code> component is rendering a{" "}
-        <code>&lt;Row /&gt;</code> component.
+        This <code>&lt;Tag /&gt;</code> component is rendering a <code>&lt;p /&gt;</code>{" "}
+        tag.
       </span>
     </Tag>
   ),
