e6e6d6bc34ac958692c25976cc5293d1954aed8b
Nicholas Lancey <nicklancey@gmail.com>
2023-11-21 10:04:54 -0800

    wip(visually-hidden-links): adds further structure
    Co-authored-by: Andrew Rubin <andrewrubin@users.noreply.github.com>

--- packages/wethegit-components/src/components/visually-hidden-links/visually-hidden-links.stories.tsx
+++ packages/wethegit-components/src/components/visually-hidden-links/visually-hidden-links.stories.tsx
@@ -2,9 +2,33 @@ import type { Meta, StoryObj } from "@storybook/react"
 
 import { VisuallyHiddenLinks } from "@local/components"
 
+const DEFAULT_AS = "ul"
+
 const meta: Meta<typeof VisuallyHiddenLinks> = {
   title: "components/visually-hidden-links",
   component: VisuallyHiddenLinks,
+  args: {
+    as: DEFAULT_AS,
+    items: [
+      {
+        label: "Skip to main navigation",
+        href: "#main-navigation",
+      },
+      {
+        label: "Skip to content",
+        href: "#main-content",
+      },
+      {
+        label: "Skip to footer",
+        href: "#footer",
+      },
+    ],
+  },
+  argTypes: {
+    as: {
+      defaultValue: { summary: DEFAULT_AS },
+    },
+  },
 } satisfies Meta<typeof VisuallyHiddenLinks>
 
 export default meta
@@ -13,22 +37,9 @@ type Story = StoryObj<typeof VisuallyHiddenLinks>
 
 export const Default: Story = {
   render: (args) => (
-    <VisuallyHiddenLinks
-      {...args}
-      items={[
-        {
-          label: "This is my little link one",
-          href: "#link-one",
-        },
-        {
-          label: "This is my little link two",
-          href: "#link-two",
-        },
-        {
-          label: "This is my little link three",
-          href: "#link-three",
-        },
-      ]}
-    />
+    <>
+      <p>TODO: Explain why nothing is shown here, and what to do</p>
+      <VisuallyHiddenLinks {...args} />
+    </>
   ),
 }
--- packages/wethegit-components/src/components/visually-hidden-links/visually-hidden-links.tsx
+++ packages/wethegit-components/src/components/visually-hidden-links/visually-hidden-links.tsx
@@ -1,19 +1,29 @@
-import { VisuallyHidden } from "@local/components"
+import { Tag, VisuallyHidden } from "@local/components"
+import type { TagProps } from "@local/components"
+import { classnames } from "@local/utilities"
 
 import styles from "./visually-hidden-links.module.scss"
+
+const DEFAULT_ELEMENT = "ul"
 
 interface Item {
   href: string
   label: string
 }
 
-type ItemProps = {
+export type ItemProps<TAs extends React.ElementType> = TagProps<TAs> & {
   items: Item[]
 }
 
-export function VisuallyHiddenLinks({ items }: ItemProps) {
+export function VisuallyHiddenLinks<TAs extends React.ElementType = "ul">({
+  items,
+  className,
+  ...props
+}: ItemProps<TAs>) {
+  const { as = DEFAULT_ELEMENT, ...rest } = props
+
   return (
-    <ul className={styles.list}>
+    <Tag as={as} className={classnames([styles.list, className])} {...rest}>
       {items.map((item, i) => {
         return (
           <li key={`${item.label}-${i}`}>
@@ -23,6 +33,6 @@ export function VisuallyHiddenLinks({ items }: ItemProps) {
           </li>
         )
       })}
-    </ul>
+    </Tag>
   )
 }
