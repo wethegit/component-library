f7a7eb40448cf815b966bc559ee93d60481b6206
Marlon Marcello <marlon.marcello@gmail.com>
2023-12-01 17:36:01 -0800

    patch(comp > modal): adds a new story with a button trigger

--- packages/wethegit-components/src/components/modal/modal.stories.tsx
+++ packages/wethegit-components/src/components/modal/modal.stories.tsx
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react"
 import { UserPreferencesProvider } from "@wethegit/react-hooks"
+import { useModal } from "@wethegit/react-modal"
+import { useRef } from "react"
 
 import { Modal } from "./modal"
 
@@ -27,12 +29,52 @@ type Story = StoryObj<typeof Modal>
 export const Default: Story = {
   render: (args) => {
     return (
-      <Modal {...args}>
+      <Modal {...args} appendToBody={false}>
         <p style={{ color: "black", margin: 0 }}>
           <strong>Close</strong> button doesn't work becayse inside the story we use the{" "}
           <code>isOpen</code> control.
         </p>
       </Modal>
+    )
+  },
+}
+
+export const WithButtonTrigger: Story = {
+  render: () => {
+    const triggerRef = useRef<HTMLButtonElement>(null)
+    const { isOpen, toggle } = useModal({
+      triggerRef,
+    })
+
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          height: "100%",
+          alignItems: "center",
+        }}
+      >
+        <button
+          ref={triggerRef}
+          onClick={toggle}
+          style={{
+            backgroundColor: "cyan",
+            padding: "20px",
+            color: "black",
+            fontFamily: "sans-serif",
+            borderRadius: "5px",
+          }}
+        >
+          {isOpen ? "Close" : "Open"} Modal
+        </button>
+        <Modal isOpen={isOpen} toggle={toggle}>
+          <p style={{ color: "black", margin: 0 }}>
+            <strong>Close</strong> button doesn't work becayse inside the story we use the{" "}
+            <code>isOpen</code> control.
+          </p>
+        </Modal>
+      </div>
     )
   },
 }
