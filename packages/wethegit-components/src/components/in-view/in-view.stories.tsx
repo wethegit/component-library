fd2543586a9d12020ea7df544a7b9c0c0f5a4e7b
Liam Egan <liam@wethecollective.com>
2024-01-11 18:09:11 -0800

    Udpating stories to include more inView items so the user has more opportunity to test.

--- packages/wethegit-components/src/components/in-view/in-view.stories.tsx
+++ packages/wethegit-components/src/components/in-view/in-view.stories.tsx
@@ -50,16 +50,38 @@ type Story = StoryObj<typeof InView>
 
 export const Default: Story = {
   render: (args) => (
-    <InView as="section" {...args}>
-      <InViewItem as="ul" animation={animation.scaleUp}>
-        <li>
-          This entire list is animated in using the <strong>"animation.scaleUp"</strong>{" "}
-          option.
-        </li>
-        <li>Fugiat quis sit minim ipsum amet non excepteur eiusmod.</li>
-        <li>Sunt adipisicing ullamco aliquip fugiat enim.</li>
-      </InViewItem>
-    </InView>
+    <>
+      <InView as="section" {...args}>
+        <InViewItem as="ul" animation={animation.scaleUp}>
+          <li>
+            This entire list is animated in using the <strong>"animation.scaleUp"</strong>{" "}
+            option.
+          </li>
+          <li>Fugiat quis sit minim ipsum amet non excepteur eiusmod.</li>
+          <li>Sunt adipisicing ullamco aliquip fugiat enim.</li>
+        </InViewItem>
+      </InView>
+      <InView as="section" {...args}>
+        <InViewItem as="ul" animation={animation.scaleUp}>
+          <li>
+            This entire list is animated in using the <strong>"animation.scaleUp"</strong>{" "}
+            option.
+          </li>
+          <li>Fugiat quis sit minim ipsum amet non excepteur eiusmod.</li>
+          <li>Sunt adipisicing ullamco aliquip fugiat enim.</li>
+        </InViewItem>
+      </InView>
+      <InView as="section" {...args}>
+        <InViewItem as="ul" animation={animation.scaleUp}>
+          <li>
+            This entire list is animated in using the <strong>"animation.scaleUp"</strong>{" "}
+            option.
+          </li>
+          <li>Fugiat quis sit minim ipsum amet non excepteur eiusmod.</li>
+          <li>Sunt adipisicing ullamco aliquip fugiat enim.</li>
+        </InViewItem>
+      </InView>
+    </>
   ),
 }
 
@@ -90,6 +112,29 @@ export const AnimationExamples: Story = {
           This uses "animation.fade"
         </InViewItem>
       </InView>
+      <InView as="section" {...args}>
+        <ul>
+          <InViewItem as="li" duration={1} animation={animation.fromLeft}>
+            The individual list items are animated in. This uses the{" "}
+            <strong>"animation.fromLeft"</strong> option.
+          </InViewItem>
+          <InViewItem as="li" duration={1} animation={animation.fromRight}>
+            This uses the <strong>"animation.fromRight"</strong> option.
+          </InViewItem>
+          <InViewItem as="li" duration={1} animation={animation.fromBottom}>
+            This uses the <strong>"animation.fromTop"</strong> option.
+          </InViewItem>
+        </ul>
+      </InView>
+
+      <InView as="ul" {...args}>
+        <InViewItem as="li" animation={animation.scaleUp}>
+          This uses "animation.scaleUp"
+        </InViewItem>
+        <InViewItem as="li" animation={animation.fade} delay={0.3}>
+          This uses "animation.fade"
+        </InViewItem>
+      </InView>
     </>
   ),
 }
@@ -97,20 +142,37 @@ export const AnimationExamples: Story = {
 export const StaggerChildren: Story = {
   name: "Auto-stagger children",
   render: (args) => (
-    <InView as="section" {...args}>
-      <InViewItem
-        as="ul"
-        staggerChildren={{ animation: animation.fromBottomFixed, stagger: 0.2, delay: 0 }}
-      >
-        <li>
-          Each list item is automatically sequenced, using the{" "}
-          <strong>staggerChildren</strong> option with a{" "}
-          <strong>animation.fromBottomFixed</strong> preset.
-        </li>
-        <li>Fugiat quis sit minim ipsum amet non excepteur eiusmod.</li>
-        <li>Sunt adipisicing ullamco aliquip fugiat enim.</li>
-        <li>Ipsum ullamco sit velit elit.</li>
-      </InViewItem>
-    </InView>
+    <>
+      <InView as="section" {...args}>
+        <InViewItem
+          as="ul"
+          staggerChildren={{ animation: animation.fromBottomFixed, stagger: 0.2, delay: 0 }}
+        >
+          <li>
+            Each list item is automatically sequenced, using the{" "}
+            <strong>staggerChildren</strong> option with a{" "}
+            <strong>animation.fromBottomFixed</strong> preset.
+          </li>
+          <li>Fugiat quis sit minim ipsum amet non excepteur eiusmod.</li>
+          <li>Sunt adipisicing ullamco aliquip fugiat enim.</li>
+          <li>Ipsum ullamco sit velit elit.</li>
+        </InViewItem>
+      </InView>
+      <InView as="section" {...args}>
+        <InViewItem
+          as="ul"
+          staggerChildren={{ animation: animation.fromBottomFixed, stagger: 0.2, delay: 0 }}
+        >
+          <li>
+            Each list item is automatically sequenced, using the{" "}
+            <strong>staggerChildren</strong> option with a{" "}
+            <strong>animation.fromBottomFixed</strong> preset.
+          </li>
+          <li>Fugiat quis sit minim ipsum amet non excepteur eiusmod.</li>
+          <li>Sunt adipisicing ullamco aliquip fugiat enim.</li>
+          <li>Ipsum ullamco sit velit elit.</li>
+        </InViewItem>
+      </InView>
+    </>
   ),
 }
