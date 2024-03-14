283e461a7cc3676cebc905247f2bc192bae80d14
Andrew Rubin <arubin78@gmail.com>
2024-03-14 13:58:17 -0700

    chore(BackToTop): prop and story cleanup

--- packages/wethegit-components/src/components/back-to-top/back-to-top.stories.tsx
+++ packages/wethegit-components/src/components/back-to-top/back-to-top.stories.tsx
@@ -6,17 +6,11 @@ const meta: Meta<typeof BackToTop> = {
   title: "components/back-to-top",
   component: BackToTop,
   args: {
-    children: "Back to top!",
     easingFunction: undefined,
     focusOnCompleteCssSelector: "#focus-me-on-complete",
     onComplete: undefined,
     pixelsPerSecond: 2000,
     revealThreshold: "85vh",
-    style: {
-      position: "sticky",
-      top: "1rem",
-      bottom: "auto",
-    },
   },
   argTypes: {
     easingFunction: {
@@ -37,11 +31,11 @@ const meta: Meta<typeof BackToTop> = {
           This story is <em>only</em> here to document all of the available props on the{" "}
           <code>BackToTop</code> component. There are a handful of customizations needed
           in order to make this component work within a Storybook Story, so please test
-          this component in-situ when contributing.
+          this component in-situ when contributing instead.
         </Text>
         <Text variant="body-legal">
-          The <code>revealThreshold</code> prop will have no effect within Storybook, as
-          the browser's default IntersectionObserver root is <code>window</code>. In
+          ⚠️ The <code>revealThreshold</code> prop will have no effect within Storybook,
+          as the browser's default IntersectionObserver root is <code>window</code>. In
           practice, if you need your back-to-top button to be scoped to a particular div
           (this is rare), please add the necessary{" "}
           <a
@@ -53,9 +47,9 @@ const meta: Meta<typeof BackToTop> = {
           to the <code>useInView</code> hook in the component itself.
         </Text>
         <Text variant="body-legal">
-          For a similar reason, the "back-to-top" scrolling on this component affects the
-          window, not the Story itself. So you will not see anything unless your window is
-          scrolled down a bit.
+          ⚠️ The "back-to-top" scrolling on this component affects the window, not the
+          Story itself. So you will not see anything unless your window is scrolled down a
+          bit. Change this within the component if you need to.
         </Text>
         <div id="focus-me-on-complete" style={{ height: "1000px", position: "relative" }}>
           <Story />
@@ -69,4 +63,17 @@ export default meta
 
 type Story = StoryObj<typeof BackToTop>
 
-export const Default: Story = {}
+export const Default: Story = {
+  render: (args) => (
+    <BackToTop
+      style={{
+        position: "sticky",
+        top: "1rem",
+        bottom: "auto",
+      }}
+      {...args}
+    >
+      Back to top!
+    </BackToTop>
+  ),
+}
