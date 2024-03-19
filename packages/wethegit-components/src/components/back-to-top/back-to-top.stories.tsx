40251b82f88ba92c914957e895044118f3488569
Andrew Rubin <arubin78@gmail.com>
2024-03-18 17:03:59 -0700

    feat(BackToTop): add tests

--- packages/wethegit-components/src/components/back-to-top/back-to-top.stories.tsx
+++ packages/wethegit-components/src/components/back-to-top/back-to-top.stories.tsx
@@ -1,4 +1,6 @@
+import { expect } from "@storybook/jest"
 import type { Meta, StoryObj } from "@storybook/react"
+import { userEvent, within } from "@storybook/testing-library"
 
 import { BackToTop, Text } from "@local/components"
 
@@ -22,18 +24,15 @@ const meta: Meta<typeof BackToTop> = {
   },
   decorators: [
     (Story) => (
-      <div
-        style={{ height: "400px", overflow: "auto", textAlign: "center" }}
-        className="childSpacing"
-      >
+      <div style={{ textAlign: "center" }} className="childSpacing">
         <Text>🎉 Yay, caveats!</Text>
-        <Text variant="body-smaller">
+        <Text variant="body">
           This story is <em>only</em> here to document all of the available props on the{" "}
           <code>BackToTop</code> component. There are a handful of customizations needed
           in order to make this component work within a Storybook Story, so please test
           this component in-situ when contributing instead.
         </Text>
-        <Text variant="body-legal">
+        <Text variant="body-smaller">
           ⚠️ The <code>revealThreshold</code> prop will have no effect within Storybook,
           as the browser's default IntersectionObserver root is <code>window</code>. In
           practice, if you need your back-to-top button to be scoped to a particular div
@@ -46,12 +45,12 @@ const meta: Meta<typeof BackToTop> = {
           </a>{" "}
           to the <code>useInView</code> hook in the component itself.
         </Text>
-        <Text variant="body-legal">
+        <Text variant="body-smaller">
           ⚠️ The "back-to-top" scrolling on this component affects the window, not the
           Story itself. So you will not see anything unless your window is scrolled down a
           bit. Change this within the component if you need to.
         </Text>
-        <div id="focus-me-on-complete" style={{ height: "1000px", position: "relative" }}>
+        <div id="focus-me-on-complete">
           <Story />
         </div>
       </div>
@@ -76,4 +75,17 @@ export const Default: Story = {
       Back to top!
     </BackToTop>
   ),
+  play: async ({ args, canvasElement }) => {
+    const canvas = within(canvasElement)
+    const b2tButton = canvas.getByText("Back to top!")
+
+    await userEvent.click(b2tButton)
+
+    // check scroll position
+    // TODO can we wait for the scroll?
+    expect(window.scrollY).toEqual(0)
+
+    // ensure the intended content receives focus
+    await expect(document.querySelector(args.focusOnCompleteCssSelector)).toHaveFocus()
+  },
 }
