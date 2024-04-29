5aae99d64d6482477f4d6202c5a711b5c7c7ea18
Andrew Rubin <arubin78@gmail.com>
2024-04-29 16:36:20 -0700

    feat(COMP: buildBreakpointClassnames): add support for 'sm' breakpoint

--- packages/wethegit-components/src/utilities/build-breakpoint-classnames/build-breakpoint-classnames.ts
+++ packages/wethegit-components/src/utilities/build-breakpoint-classnames/build-breakpoint-classnames.ts
@@ -1,16 +1,19 @@
 import type { ClassnamesProps } from "../classnames"
 
 export function buildBreakpointClassnames<T extends string | number | boolean>(
-  prop: T | Partial<Omit<Breakpoints<T>, "sm">> | undefined,
+  prop: T | Partial<Breakpoints<T>> | undefined,
   styles: Record<string, string>,
   styleName: string
 ): ClassnamesProps {
   const allClassnames: ClassnamesProps = []
 
   if (typeof prop === "object") {
-    const { md, lg, xl, xxl } = prop
+    const { sm, md, lg, xl, xxl } = prop
 
-    if (md) allClassnames.push(styles[`${styleName}-${md}`])
+    const mediumPrefix = sm ? "md-" : ""
+
+    if (sm) allClassnames.push(styles[`${styleName}-${sm}`])
+    if (md) allClassnames.push(styles[`${styleName}-${mediumPrefix}${md}`])
     if (lg) allClassnames.push(styles[`${styleName}-lg-${lg}`])
     if (xl) allClassnames.push(styles[`${styleName}-xl-${xl}`])
     if (xxl) allClassnames.push(styles[`${styleName}-xxl-${xxl}`])
