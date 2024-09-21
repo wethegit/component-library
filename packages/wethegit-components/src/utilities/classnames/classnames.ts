088e9b0bb76cef0b43ad814a5a97c218480ebee1
Clinton <80900245+pravton@users.noreply.github.com>
2024-09-20 20:03:39 -0400

    feature(classnames-test) improved the classnames logic and added suport for double nested array

--- packages/wethegit-components/src/utilities/classnames/classnames.ts
+++ packages/wethegit-components/src/utilities/classnames/classnames.ts
@@ -1,11 +1,25 @@
-export type ClassnamesProps = boolean | string | undefined | null | ClassnamesProps[]
+export type ClassnamesProps =
+  | boolean
+  | string
+  | number
+  | object
+  | undefined
+  | null
+  | ClassnamesProps[]
 
 export function classnames(classes: ClassnamesProps): string {
-  if (!classes || typeof classes === "boolean") return ""
+  if (
+    !classes ||
+    typeof classes === "boolean" ||
+    typeof classes === "number" ||
+    (typeof classes === "object" && !Array.isArray(classes))
+  )
+    return ""
+
   if (!Array.isArray(classes)) return classes
 
   return classes
-    .flat()
+    .flat(2)
     .filter((c) => typeof c === "string")
     .join(" ")
 }
