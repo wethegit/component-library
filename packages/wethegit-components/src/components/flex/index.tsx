b9ec376d1f85dd686a16190e7426b0da30a5c193
Andrew Rubin <arubin78@gmail.com>
2026-06-18 18:18:12 -0700

    feat(COMP: Flex): remove reverse prop, add support for breakpoint-specific flexDirection prop, fix JSDoc

--- packages/wethegit-components/src/components/flex/index.tsx
+++ packages/wethegit-components/src/components/flex/index.tsx
@@ -2,7 +2,9 @@ export { Flex } from "./flex"
 export type {
   AlignBreakpoints,
   BooleanBreakpoints,
+  DirectionBreakpoints,
   FlexAlign,
+  FlexDirection,
   FlexJustify,
   FlexProps,
   JustifyBreakpoints,
